521e4900295ad60787d2433ca7557099032f1914
Asude Fışkın <74365269+asuf29@users.noreply.github.com>
2023-01-28 16:52:25 +0300

    header section created

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        lg: "1140px",
+        xl: "1140px",
+        "2xl": "1140px",
+      },
+    },
+
+    extend: {
+      fontFamily: {
+        gemunu: ["Gemunu Libre", "sans-serif"],
+        open: ["Open Sans", "sans-serif"],
+      },
+
+      colors: {
+        "tw-red": "#BC1A45",
+        "tw-melon": "#FFD369",
+        "tw-grey": "#DDDDDD",
+        "tw-white": "#F7F7F7",
+        "tw-blue": "#00ACEE",
+      },
+    },
   },
   plugins: [],
 };
